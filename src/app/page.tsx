1fe469d59fee9ad54fca1287b8dcefa88871191b
katai <info@katai.io>
2024-12-31 21:04:20 -0500

    Fix mobile scrolling issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <ClientLayout>
-      <main className="w-full overflow-x-hidden">
+      <main>
         <Header />
         <Hero />
         <Gallery />
